983181197db8b9a2435c2a0f338f7b7054843382
danil <smexhehe@gmail.com>
2026-04-28 22:21:35 +0300

    get put(start)

---
+++ go.mod
@@ -0,0 +1,3 @@
+module TODO
+
+go 1.25.5
--- main.go
+++ main.go
@@ -4,27 +4,70 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type Task struct {
 	ID    int    `json:"id"`
 	Title string `json:"title"`
+	Done  bool   `json:"done"`
 }
 
 var Tasks []Task
 
 func taskHandler(w http.ResponseWriter, r *http.Request) {
-	jsonData, err := json.Marshal(Tasks)
-	if err != nil {
-		fmt.Println("Error marshaling:", err)
+
+	switch r.Method {
+	case http.MethodGet:
+		jsonData, err := json.Marshal(Tasks)
+		if err != nil {
+			http.Error(w, "Error marshaling", 500)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonData)
+
+	case http.MethodPost:
+		var newTask Task
+		err := json.NewDecoder(r.Body).Decode(&newTask)
+		if err != nil {
+			http.Error(w, "Error marshaling", 500)
+			return
+		}
+		newTask.ID = len(Tasks) + 1
+		Tasks = append(Tasks, newTask)
+		jsonDataAccept, err := json.Marshal(newTask)
+		if err != nil {
+			http.Error(w, "Error marshaling", 400)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonDataAccept)
+	default:
+		http.Error(w, "method not allowed", 405)
+
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsonData)
+
+}
+
+func taskByIDHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/task/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "id not number", 400)
+	}
+
+	switch r.Method {
+	case http.MethodPut:
+
+	}
 }
 
 func main() {
 
 	http.HandleFunc("/task", taskHandler)
+	http.HandleFunc("/task/", taskByIDHandler)
 
 	fmt.Println("Start server")
 
